984c5702721781c837054d35adb3edd9aee0ebd2
Franky Solano <fsolano@nevada.unr.edu>
2018-05-26 16:06:53 -0700

    initial source code commit

---
+++ LoginPortal/Models/User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPortal.Models
+{
+    public class User : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+                OnPropertyChanged("FullName");
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged();
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        public string FullName { get => FirstName + " " + LastName; }
+
+        public User()
+        {
+
+        }
+
+        public User(string firstName, string lastname)
+        {
+            FirstName = firstName;
+            LastName = lastname;
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
